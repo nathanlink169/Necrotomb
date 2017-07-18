16af57d97ac48b52ac1985d6d5eebccf77601d10
Evan Campbell <evan.campbell1218@gmail.com>
2017-07-18 16:24:57 -0400

    Work in progress upgrading SaveData functionality
    - SaveData data now has bitmask ints to store unique flags of data.
    - SaveData class now has helper functions that perform bitwise manipulations to the save data.
    - GSaveManager now keeps track of unique data pools for storing save data, as well as unique flags set for all current items and game events.
    - GSaveManager now reads and writes a json file to access SaveData instead of PlayerPrefs which was previously used.

--- Assets/Gameplay Scripts/DataContainers/SaveData.cs
+++ Assets/Gameplay Scripts/DataContainers/SaveData.cs
@@ -4,57 +4,155 @@ using GameFramework;
 public class SaveData
 {
     public int SaveDataIndex;
+
+    public int[] DataPools;
+
     public int SecurityClearance = -1;
-
-    public bool UnlockedREI = false;
-    public bool UpgradedREI = false;
-    public bool UnlockedGuidedRocket = false;
-    public bool UpgradedGuidedRocket = false;
-
-    public bool UnlockedThrustJump = false;
-    public bool UpgradedThrustJump = false;
-    public bool UnlockedGrappleHook = false;
-    public bool UpgradedGrappleHook = false;
-
-    public bool HealthUpgrade1 = false;
-    public bool HealthUpgrade2 = false;
-    public bool HealthUpgrade3 = false;
-    public bool HealthUpgrade4 = false;
-
-    public bool DeathHintActivated = false;
-    public bool DefeatedBoss = false;
 
     public LoadPoint CurrentSavePoint = LoadPoint.AreaMRoom1;
 
     public SaveData(int in_iSaveDataIndex)
     {
-        SaveDataIndex = in_iSaveDataIndex;
+        ResetSaveData(in_iSaveDataIndex);
     }
 
-    public SaveData(int in_iSaveDataIndex, Dictionary<string, object> in_data)
+    #region Helper methods for checking if various items are unlocked
+    // This should be the only place you need to change if you want to switch what pools items are in.
+
+    // Unique items
+    public bool UnlockedREI
+    {
+        get { return UnlockedItem(GSaveManager.DataPoolID.UNIQUE_ITEMS, GSaveManager.UNLOCKED_REI_FLAG); }
+    }
+    public bool UpgradedREI
+    {
+        get { return UnlockedItem(GSaveManager.DataPoolID.UNIQUE_ITEMS, GSaveManager.UPGRADED_REI_FLAG); }
+    }
+    public bool UnlockedGuidedRocket
+    {
+        get { return UnlockedItem(GSaveManager.DataPoolID.UNIQUE_ITEMS, GSaveManager.UNLOCKED_GUIDED_ROCKET_FLAG); }
+    }
+    public bool UpgradedGuidedRocket
+    {
+        get { return UnlockedItem(GSaveManager.DataPoolID.UNIQUE_ITEMS, GSaveManager.UPGRADED_REI_FLAG); }
+    }
+    public bool UnlockedThrustJump
+    {
+        get { return UnlockedItem(GSaveManager.DataPoolID.UNIQUE_ITEMS, GSaveManager.UNLOCKED_THRUST_JUMP_FLAG); }
+    }
+    public bool UpgradedThrustJump
+    {
+        get { return UnlockedItem(GSaveManager.DataPoolID.UNIQUE_ITEMS, GSaveManager.UPGRADED_THRUST_JUMP_FLAG); }
+    }
+    public bool UnlockedGrappleHook
+    {
+        get { return UnlockedItem(GSaveManager.DataPoolID.UNIQUE_ITEMS, GSaveManager.UNLOCKED_GRAPPLE_HOOK_FLAG); }
+    }
+    public bool UpgradedGrappleHook
+    {
+        get { return UnlockedItem(GSaveManager.DataPoolID.UNIQUE_ITEMS, GSaveManager.UPGRADED_GRAPPLE_HOOK_FLAG); }
+    }
+
+    // Story event triggers
+    public bool DeathHintActivated
+    {
+        get { return UnlockedItem(GSaveManager.DataPoolID.STORY_EVENTS, GSaveManager.DEATH_HINT_ACTIVATED_FLAG); }
+    }
+    public bool DefeatedBoss
+    {
+        get { return UnlockedItem(GSaveManager.DataPoolID.STORY_EVENTS, GSaveManager.DEFEATED_BOSS_FLAG); }
+    }
+
+    // Health upgrades
+    public bool UnlockedHealthUpgrade(int in_iDataFlag)
+    {
+        return (UnlockedItem(GSaveManager.DataPoolID.HEALTH_UPGRADES, in_iDataFlag));
+    }
+    //public bool HealthUpgrade1 = false;
+    //public bool HealthUpgrade2 = false;
+    //public bool HealthUpgrade3 = false;
+    //public bool HealthUpgrade4 = false;
+    #endregion
+
+    #region SaveData Manipulation
+
+    /// <summary>
+    /// Returns true if an item from one item pool has been collected, false otherwise.
+    /// </summary>
+    /// <param name="in_iDataPool"></param>
+    /// <param name="in_iDataFlag"></param>
+    /// <returns></returns>
+    public bool UnlockedItem(GSaveManager.DataPoolID in_iDataPool, int in_iDataFlag)
+    {
+        if (in_iDataPool < 0 || (int)in_iDataPool >= DataPools.Length)
+            return false;
+
+        int dataBitmask = DataPools[(int)in_iDataPool];
+
+        return (dataBitmask & in_iDataFlag) != 0;
+    }
+
+    /// <summary>
+    /// Sets a bit flag in the data pool bitmask.
+    /// Use this to lock or unlock an item.
+    /// </summary>
+    /// <param name="in_iDataPool"></param>
+    /// <param name="in_iDataFlag"></param>
+    public void SetItem(GSaveManager.DataPoolID in_iDataPool, int in_iDataFlag)
+    {
+        if (in_iDataPool < 0 || (int)in_iDataPool >= DataPools.Length)
+            return;
+
+        DataPools[(int)in_iDataPool] |= 1 << in_iDataFlag;
+    }
+
+    /// <summary>
+    /// Sets a bit flag in the data pool bitmask to 0.
+    /// </summary>
+    /// <param name="in_iDataPool"></param>
+    /// <param name="in_iDataFlag"></param>
+    public void ClearItem(int in_iDataPool, int in_iDataFlag)
+    {
+        if (in_iDataPool < 0 || in_iDataPool >= DataPools.Length)
+            return;
+
+        DataPools[in_iDataPool] &= ~(1 << in_iDataFlag);
+    }
+
+    /// <summary>
+    /// Toggles a bit flag in the data pool bitmask.
+    /// </summary>
+    /// <param name="in_iDataPool"></param>
+    /// <param name="in_iDataFlag"></param>
+    public void ToggleItem(int in_iDataPool, int in_iDataFlag)
+    {
+        if (in_iDataPool < 0 || in_iDataPool >= DataPools.Length)
+            return;
+
+        DataPools[in_iDataPool] ^= 1 << in_iDataFlag;
+    }
+
+    /// <summary>
+    /// Clears all data in current SaveData.
+    /// </summary>
+    public void ResetSaveData(int in_iSaveDataIndex)
     {
         SaveDataIndex = in_iSaveDataIndex;
 
-        SecurityClearance = (int)in_data[GSaveManager.SECURITY_CLEARANCE];
+        if (DataPools == null)
+        {
+            DataPools = new int[(int)GSaveManager.DataPoolID.MAX_POOL_IDS];
+        }
 
-        UnlockedREI = (bool)in_data[GSaveManager.UNLOCKED_REI];
-        UpgradedREI = (bool)in_data[GSaveManager.UPGRADED_REI];
-        UnlockedGuidedRocket = (bool)in_data[GSaveManager.UNLOCKED_GUIDED_ROCKET];
-        UpgradedGuidedRocket = (bool)in_data[GSaveManager.UPGRADED_GUIDED_ROCKET];
+        for (int i = 0; i < DataPools.Length; i++)
+        {
+            DataPools[i] = 0;
+        }
 
-        UnlockedThrustJump = (bool)in_data[GSaveManager.UNLOCKED_THRUST_JUMP];
-        UpgradedThrustJump = (bool)in_data[GSaveManager.UPGRADED_THRUST_JUMP];
-        UnlockedGrappleHook = (bool)in_data[GSaveManager.UNLOCKED_GRAPPLE_HOOK];
-        UpgradedGrappleHook = (bool)in_data[GSaveManager.UPGRADED_THRUST_JUMP];
+        SecurityClearance = -1;
 
-        HealthUpgrade1 = (bool)in_data[GSaveManager.UNLOCKED_REI];
-        HealthUpgrade2 = (bool)in_data[GSaveManager.UPGRADED_REI];
-        HealthUpgrade3 = (bool)in_data[GSaveManager.UNLOCKED_REI];
-        HealthUpgrade4 = (bool)in_data[GSaveManager.UPGRADED_REI];
-
-        DeathHintActivated = (bool)in_data[GSaveManager.UPGRADED_REI];
-        DefeatedBoss = (bool)in_data[GSaveManager.DEFEATED_BOSS];
-
-        CurrentSavePoint = (LoadPoint)in_data[GSaveManager.SAVE_POINT];
+        CurrentSavePoint = LoadPoint.AreaMRoom1;
     }
+
+    #endregion
 }
--- Assets/Gameplay Scripts/Framework/SaveLoad/GSaveMgr.cs
+++ Assets/Gameplay Scripts/Framework/SaveLoad/GSaveMgr.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 namespace GameFramework
@@ -28,38 +30,127 @@ namespace GameFramework
         public const string SECURITY_CLEARANCE = "SECURITY_CLEARANCE";
 
         private const string SAVE_DATA_EXISTS = "EXISTS";
+
+        // The following flags represent bits that can be set in the save data.
+        // For one-time collectibles in-game, give them an appropriate pool and a unique flag.
+        // Using bitmasks, you can store the values of 31 unique flags in a single bitmask int.
+        #region Optimization Notice
+        // Optimization note: If you require an expansive number of unique flags to be stored,
+        // (over 31 unique flags in a pool), you may consider switching to longs, or use multiple pools
+        // at the cost of increase memory usage. Likewise, if you find that you never have more than 15
+        // flags in each pool, switch to shorts to decrease memory usage. You could potentially put all flags
+        // into a single pool if you have < 32 total for an int or < 64 total for a long.
+        // If more space is needed, create more pools.
+        #endregion
+
+        // Pool IDs
+        public enum DataPoolID
+        {
+            UNIQUE_ITEMS,
+            HEALTH_UPGRADES,
+            STORY_EVENTS,
+            MAX_POOL_IDS
+        }
+
+        // Unique item pool
+        public const int UNLOCKED_REI_FLAG =            1 << 0;
+        public const int UPGRADED_REI_FLAG =            1 << 1;
+        public const int UNLOCKED_GUIDED_ROCKET_FLAG =  1 << 2;
+        public const int UPGRADED_GUIDED_ROCKET_FLAG =  1 << 3;
+        public const int UNLOCKED_THRUST_JUMP_FLAG =    1 << 4;
+        public const int UPGRADED_THRUST_JUMP_FLAG =    1 << 5;
+        public const int UNLOCKED_GRAPPLE_HOOK_FLAG =   1 << 6;
+        public const int UPGRADED_GRAPPLE_HOOK_FLAG =   1 << 7;
+
+        // Health upgrade pool
+        public const int HEALTH_UPGRADE_1_FLAG =        1 << 0;
+        public const int HEALTH_UPGRADE_2_FLAG =        1 << 1;
+        public const int HEALTH_UPGRADE_3_FLAG =        1 << 2;
+        public const int HEALTH_UPGRADE_4_FLAG =        1 << 3;
+
+        // Story event pool
+        public const int DEATH_HINT_ACTIVATED_FLAG =    1 << 0;
+        public const int DEFEATED_BOSS_FLAG =           1 << 1;
+
         #endregion
 
         #region Public
+
+        /// <summary>
+        /// Returns the file path of the save data at index, or null if the index is invalid.
+        /// </summary>
+        /// <param name="in_iSaveDataIndex">Save data index</param>
+        /// <returns>Save data file path</returns>
+        public static string GetSaveFilePath(int in_iSaveDataIndex)
+        {
+            // Error checking
+            if (in_iSaveDataIndex <= 0 || in_iSaveDataIndex > MaxSaveFileCount)
+            {
+                GDebug.LogError("GetSaveFilePath: Invalid SaveData index - " + in_iSaveDataIndex);
+                return null;
+            }
+
+            return SaveDataPaths[in_iSaveDataIndex];
+        }
+
         public static bool FileExist(int in_iSaveData)
         {
-            return GetBool(SAVE_DATA_EXISTS + in_iSaveData.ToString());
+            string SaveFilePath = GetSaveFilePath(in_iSaveData);
+            if (SaveFilePath == null)
+            {
+                return false;
+            }
+
+            return (File.Exists(SaveFilePath));
+            //return GetBool(SAVE_DATA_EXISTS + in_iSaveData.ToString());
         }
 
         public static void Save(SaveData in_saveData)
         {
-            SetBool(SAVE_DATA_EXISTS + in_saveData.SaveDataIndex.ToString(), true);
+            if (in_saveData == null || in_saveData.SaveDataIndex < 0 || in_saveData.SaveDataIndex >= MaxSaveFileCount)
+                return;
 
-            SetInt(SECURITY_CLEARANCE, in_saveData.SecurityClearance);
-            SetInt(SAVE_POINT, (int)in_saveData.CurrentSavePoint);
+            try
+            {
+                string json = JsonUtility.ToJson(in_saveData);
 
-            SetBool(UNLOCKED_REI + in_saveData.SaveDataIndex.ToString(), in_saveData.UnlockedREI);
-            SetBool(UPGRADED_REI + in_saveData.SaveDataIndex.ToString(), in_saveData.UpgradedREI);
-            SetBool(UNLOCKED_GUIDED_ROCKET + in_saveData.SaveDataIndex.ToString(), in_saveData.UnlockedGuidedRocket);
-            SetBool(UPGRADED_GUIDED_ROCKET + in_saveData.SaveDataIndex.ToString(), in_saveData.UpgradedGuidedRocket);
+                string filePath = GetSaveFilePath(in_saveData.SaveDataIndex);
 
-            SetBool(UNLOCKED_THRUST_JUMP + in_saveData.SaveDataIndex.ToString(), in_saveData.UnlockedThrustJump);
-            SetBool(UPGRADED_THRUST_JUMP + in_saveData.SaveDataIndex.ToString(), in_saveData.UpgradedThrustJump);
-            SetBool(UNLOCKED_GRAPPLE_HOOK + in_saveData.SaveDataIndex.ToString(), in_saveData.UnlockedGrappleHook);
-            SetBool(UPGRADED_GRAPPLE_HOOK + in_saveData.SaveDataIndex.ToString(), in_saveData.UpgradedGrappleHook);
+                GDebug.Log("Saving game data to file: " + filePath);
+                
+                StreamWriter file = new StreamWriter(filePath);
+                file.WriteLine(json);
+                file.Close();
+            }
+            catch(Exception ex)
+            {
+                GDebug.LogError("Error writing file - " + SaveDataPaths[in_saveData.SaveDataIndex] + ":\n" + ex.Message);
+            }
 
-            SetBool(UNLOCKED_HEALTH_UPGRADE_1 + in_saveData.SaveDataIndex.ToString(), in_saveData.HealthUpgrade1);
-            SetBool(UNLOCKED_HEALTH_UPGRADE_2 + in_saveData.SaveDataIndex.ToString(), in_saveData.HealthUpgrade2);
-            SetBool(UNLOCKED_HEALTH_UPGRADE_3 + in_saveData.SaveDataIndex.ToString(), in_saveData.HealthUpgrade3);
-            SetBool(UNLOCKED_HEALTH_UPGRADE_4 + in_saveData.SaveDataIndex.ToString(), in_saveData.HealthUpgrade4);
+            #region Save (Old PlayerPrefs code)
+            //SetBool(SAVE_DATA_EXISTS + in_saveData.SaveDataIndex.ToString(), true);
 
-            SetBool(DEATH_HINT_ACTIVATED + in_saveData.SaveDataIndex.ToString(), in_saveData.DeathHintActivated);
-            SetBool(DEFEATED_BOSS + in_saveData.SaveDataIndex.ToString(), in_saveData.DefeatedBoss);
+            //SetInt(SECURITY_CLEARANCE, in_saveData.SecurityClearance);
+            //SetInt(SAVE_POINT, (int)in_saveData.CurrentSavePoint);
+
+            //SetBool(UNLOCKED_REI + in_saveData.SaveDataIndex.ToString(), in_saveData.UnlockedREI);
+            //SetBool(UPGRADED_REI + in_saveData.SaveDataIndex.ToString(), in_saveData.UpgradedREI);
+            //SetBool(UNLOCKED_GUIDED_ROCKET + in_saveData.SaveDataIndex.ToString(), in_saveData.UnlockedGuidedRocket);
+            //SetBool(UPGRADED_GUIDED_ROCKET + in_saveData.SaveDataIndex.ToString(), in_saveData.UpgradedGuidedRocket);
+
+            //SetBool(UNLOCKED_THRUST_JUMP + in_saveData.SaveDataIndex.ToString(), in_saveData.UnlockedThrustJump);
+            //SetBool(UPGRADED_THRUST_JUMP + in_saveData.SaveDataIndex.ToString(), in_saveData.UpgradedThrustJump);
+            //SetBool(UNLOCKED_GRAPPLE_HOOK + in_saveData.SaveDataIndex.ToString(), in_saveData.UnlockedGrappleHook);
+            //SetBool(UPGRADED_GRAPPLE_HOOK + in_saveData.SaveDataIndex.ToString(), in_saveData.UpgradedGrappleHook);
+
+            //SetBool(UNLOCKED_HEALTH_UPGRADE_1 + in_saveData.SaveDataIndex.ToString(), in_saveData.HealthUpgrade1);
+            //SetBool(UNLOCKED_HEALTH_UPGRADE_2 + in_saveData.SaveDataIndex.ToString(), in_saveData.HealthUpgrade2);
+            //SetBool(UNLOCKED_HEALTH_UPGRADE_3 + in_saveData.SaveDataIndex.ToString(), in_saveData.HealthUpgrade3);
+            //SetBool(UNLOCKED_HEALTH_UPGRADE_4 + in_saveData.SaveDataIndex.ToString(), in_saveData.HealthUpgrade4);
+
+            //SetBool(DEATH_HINT_ACTIVATED + in_saveData.SaveDataIndex.ToString(), in_saveData.DeathHintActivated);
+            //SetBool(DEFEATED_BOSS + in_saveData.SaveDataIndex.ToString(), in_saveData.DefeatedBoss);
+            #endregion
         }
 
         public static SaveData Load(int in_iSaveDataIndex)
@@ -67,55 +158,109 @@ namespace GameFramework
             if (!FileExist(in_iSaveDataIndex))
                 return new SaveData(in_iSaveDataIndex);
 
-            Dictionary<string, object> objects = new Dictionary<string, object>();
-            objects.Add(UNLOCKED_REI, GetBool(UNLOCKED_REI + in_iSaveDataIndex.ToString()));
-            objects.Add(UPGRADED_REI, GetBool(UPGRADED_REI + in_iSaveDataIndex.ToString()));
-            objects.Add(UNLOCKED_GUIDED_ROCKET, GetBool(UNLOCKED_GUIDED_ROCKET + in_iSaveDataIndex.ToString()));
-            objects.Add(UPGRADED_GUIDED_ROCKET, GetBool(UPGRADED_GUIDED_ROCKET + in_iSaveDataIndex.ToString()));
+            SaveData toReturn = null;
 
-            objects.Add(UNLOCKED_THRUST_JUMP, GetBool(UNLOCKED_THRUST_JUMP + in_iSaveDataIndex.ToString()));
-            objects.Add(UPGRADED_THRUST_JUMP, GetBool(UPGRADED_THRUST_JUMP + in_iSaveDataIndex.ToString()));
-            objects.Add(UNLOCKED_GRAPPLE_HOOK, GetBool(UNLOCKED_GRAPPLE_HOOK + in_iSaveDataIndex.ToString()));
-            objects.Add(UPGRADED_GRAPPLE_HOOK, GetBool(UPGRADED_GRAPPLE_HOOK + in_iSaveDataIndex.ToString()));
+            try
+            {
+                GDebug.Log("Reading game data from file: " + SaveDataPaths[in_iSaveDataIndex]);
 
-            objects.Add(UNLOCKED_HEALTH_UPGRADE_1, GetBool(UNLOCKED_HEALTH_UPGRADE_1 + in_iSaveDataIndex.ToString()));
-            objects.Add(UNLOCKED_HEALTH_UPGRADE_2, GetBool(UNLOCKED_HEALTH_UPGRADE_2 + in_iSaveDataIndex.ToString()));
-            objects.Add(UNLOCKED_HEALTH_UPGRADE_3, GetBool(UNLOCKED_HEALTH_UPGRADE_3 + in_iSaveDataIndex.ToString()));
-            objects.Add(UNLOCKED_HEALTH_UPGRADE_4, GetBool(UNLOCKED_HEALTH_UPGRADE_4 + in_iSaveDataIndex.ToString()));
+                StreamReader file = new StreamReader(SaveDataPaths[in_iSaveDataIndex]);
+                string json = file.ReadToEnd();
+                file.Close();
 
-            objects.Add(DEATH_HINT_ACTIVATED, GetBool(DEATH_HINT_ACTIVATED + in_iSaveDataIndex.ToString()));
-            objects.Add(DEFEATED_BOSS, GetBool(DEFEATED_BOSS + in_iSaveDataIndex.ToString()));
+                toReturn = JsonUtility.FromJson<SaveData>(json);
+            }
+            catch (Exception ex)
+            {
+                GDebug.LogError("Error reading file - " + SaveDataPaths[in_iSaveDataIndex] + ":\n" + ex.Message);
+            }
 
-            objects.Add(SAVE_POINT, GetInt(SAVE_POINT));
-            objects.Add(SECURITY_CLEARANCE, GetInt(SECURITY_CLEARANCE));
+            // toReturn could possibly be null after file read
+            if (toReturn == null)
+            {
+                toReturn = new SaveData(in_iSaveDataIndex);
+            }
+            // If we do have a valid save file, perform any validations needed in here.
+            else
+            {
+                // Make sure DataPools are valid
+                if (toReturn.DataPools == null || toReturn.DataPools.Length != (int)DataPoolID.MAX_POOL_IDS)
+                {
+                    // TODO If the data is invalid, should we cancel loading the file? Reset the save data?
+                }
+            }
 
-            SaveData toReturn = new SaveData(in_iSaveDataIndex, objects);
+            #region Load (Old PlayerPrefs code)
+            //Dictionary<string, object> objects = new Dictionary<string, object>();
+            //objects.Add(UNLOCKED_REI, GetBool(UNLOCKED_REI + in_iSaveDataIndex.ToString()));
+            //objects.Add(UPGRADED_REI, GetBool(UPGRADED_REI + in_iSaveDataIndex.ToString()));
+            //objects.Add(UNLOCKED_GUIDED_ROCKET, GetBool(UNLOCKED_GUIDED_ROCKET + in_iSaveDataIndex.ToString()));
+            //objects.Add(UPGRADED_GUIDED_ROCKET, GetBool(UPGRADED_GUIDED_ROCKET + in_iSaveDataIndex.ToString()));
+
+            //objects.Add(UNLOCKED_THRUST_JUMP, GetBool(UNLOCKED_THRUST_JUMP + in_iSaveDataIndex.ToString()));
+            //objects.Add(UPGRADED_THRUST_JUMP, GetBool(UPGRADED_THRUST_JUMP + in_iSaveDataIndex.ToString()));
+            //objects.Add(UNLOCKED_GRAPPLE_HOOK, GetBool(UNLOCKED_GRAPPLE_HOOK + in_iSaveDataIndex.ToString()));
+            //objects.Add(UPGRADED_GRAPPLE_HOOK, GetBool(UPGRADED_GRAPPLE_HOOK + in_iSaveDataIndex.ToString()));
+
+            //objects.Add(UNLOCKED_HEALTH_UPGRADE_1, GetBool(UNLOCKED_HEALTH_UPGRADE_1 + in_iSaveDataIndex.ToString()));
+            //objects.Add(UNLOCKED_HEALTH_UPGRADE_2, GetBool(UNLOCKED_HEALTH_UPGRADE_2 + in_iSaveDataIndex.ToString()));
+            //objects.Add(UNLOCKED_HEALTH_UPGRADE_3, GetBool(UNLOCKED_HEALTH_UPGRADE_3 + in_iSaveDataIndex.ToString()));
+            //objects.Add(UNLOCKED_HEALTH_UPGRADE_4, GetBool(UNLOCKED_HEALTH_UPGRADE_4 + in_iSaveDataIndex.ToString()));
+
+            //objects.Add(DEATH_HINT_ACTIVATED, GetBool(DEATH_HINT_ACTIVATED + in_iSaveDataIndex.ToString()));
+            //objects.Add(DEFEATED_BOSS, GetBool(DEFEATED_BOSS + in_iSaveDataIndex.ToString()));
+
+            //objects.Add(SAVE_POINT, GetInt(SAVE_POINT));
+            //objects.Add(SECURITY_CLEARANCE, GetInt(SECURITY_CLEARANCE));
+
+            //SaveData toReturn = new SaveData(in_iSaveDataIndex, objects);
+            #endregion
+
             return toReturn;
         }
 
         public static void Delete(int in_iSaveDataIndex)
         {
-            PlayerPrefs.DeleteKey(UNLOCKED_REI + in_iSaveDataIndex.ToString());
-            PlayerPrefs.DeleteKey(UPGRADED_REI + in_iSaveDataIndex.ToString());
-            PlayerPrefs.DeleteKey(UNLOCKED_GUIDED_ROCKET + in_iSaveDataIndex.ToString());
-            PlayerPrefs.DeleteKey(UPGRADED_GUIDED_ROCKET + in_iSaveDataIndex.ToString());
+            try
+            {
+                if (!FileExist(in_iSaveDataIndex))
+                    return;
 
-            PlayerPrefs.DeleteKey(UNLOCKED_THRUST_JUMP + in_iSaveDataIndex.ToString());
-            PlayerPrefs.DeleteKey(UPGRADED_THRUST_JUMP + in_iSaveDataIndex.ToString());
-            PlayerPrefs.DeleteKey(UNLOCKED_GRAPPLE_HOOK + in_iSaveDataIndex.ToString());
-            PlayerPrefs.DeleteKey(UPGRADED_GRAPPLE_HOOK + in_iSaveDataIndex.ToString());
+                string filePath = GetSaveFilePath(in_iSaveDataIndex);
 
-            PlayerPrefs.DeleteKey(UNLOCKED_HEALTH_UPGRADE_1 + in_iSaveDataIndex.ToString());
-            PlayerPrefs.DeleteKey(UNLOCKED_HEALTH_UPGRADE_2 + in_iSaveDataIndex.ToString());
-            PlayerPrefs.DeleteKey(UNLOCKED_HEALTH_UPGRADE_3 + in_iSaveDataIndex.ToString());
-            PlayerPrefs.DeleteKey(UNLOCKED_HEALTH_UPGRADE_4 + in_iSaveDataIndex.ToString());
+                if (File.Exists(filePath))
+                {
+                    File.Delete(filePath);
+                }
+            }
+            catch (Exception ex)
+            {
+                GDebug.LogError("Error while deleting save file - " + SaveDataPaths[in_iSaveDataIndex] + ":\n" + ex.Message);
+            }
 
-            PlayerPrefs.DeleteKey(DEATH_HINT_ACTIVATED + in_iSaveDataIndex.ToString());
-            PlayerPrefs.DeleteKey(DEFEATED_BOSS + in_iSaveDataIndex.ToString());
-            PlayerPrefs.DeleteKey(SAVE_POINT + in_iSaveDataIndex.ToString());
-            PlayerPrefs.DeleteKey(SECURITY_CLEARANCE + in_iSaveDataIndex.ToString());
+            #region Delete (Old PlayerPrefs code)
+            //PlayerPrefs.DeleteKey(UNLOCKED_REI + in_iSaveDataIndex.ToString());
+            //PlayerPrefs.DeleteKey(UPGRADED_REI + in_iSaveDataIndex.ToString());
+            //PlayerPrefs.DeleteKey(UNLOCKED_GUIDED_ROCKET + in_iSaveDataIndex.ToString());
+            //PlayerPrefs.DeleteKey(UPGRADED_GUIDED_ROCKET + in_iSaveDataIndex.ToString());
 
-            PlayerPrefs.DeleteKey(SAVE_DATA_EXISTS + in_iSaveDataIndex.ToString());
+            //PlayerPrefs.DeleteKey(UNLOCKED_THRUST_JUMP + in_iSaveDataIndex.ToString());
+            //PlayerPrefs.DeleteKey(UPGRADED_THRUST_JUMP + in_iSaveDataIndex.ToString());
+            //PlayerPrefs.DeleteKey(UNLOCKED_GRAPPLE_HOOK + in_iSaveDataIndex.ToString());
+            //PlayerPrefs.DeleteKey(UPGRADED_GRAPPLE_HOOK + in_iSaveDataIndex.ToString());
+
+            //PlayerPrefs.DeleteKey(UNLOCKED_HEALTH_UPGRADE_1 + in_iSaveDataIndex.ToString());
+            //PlayerPrefs.DeleteKey(UNLOCKED_HEALTH_UPGRADE_2 + in_iSaveDataIndex.ToString());
+            //PlayerPrefs.DeleteKey(UNLOCKED_HEALTH_UPGRADE_3 + in_iSaveDataIndex.ToString());
+            //PlayerPrefs.DeleteKey(UNLOCKED_HEALTH_UPGRADE_4 + in_iSaveDataIndex.ToString());
+
+            //PlayerPrefs.DeleteKey(DEATH_HINT_ACTIVATED + in_iSaveDataIndex.ToString());
+            //PlayerPrefs.DeleteKey(DEFEATED_BOSS + in_iSaveDataIndex.ToString());
+            //PlayerPrefs.DeleteKey(SAVE_POINT + in_iSaveDataIndex.ToString());
+            //PlayerPrefs.DeleteKey(SECURITY_CLEARANCE + in_iSaveDataIndex.ToString());
+
+            //PlayerPrefs.DeleteKey(SAVE_DATA_EXISTS + in_iSaveDataIndex.ToString());
+            #endregion
         }
         #endregion
 
@@ -155,6 +300,42 @@ namespace GameFramework
         {
             PlayerPrefs.SetInt(in_key, in_value ? 1 : 0);
         }
+        #endregion
+
+        #region Generated Save File Paths
+
+        // Gets the save data file paths.
+        // First time this is called, the paths get generated.
+        public static string[] SaveDataPaths
+        {
+            get
+            {
+                // Generate file paths once
+                if (_SaveDataPaths == null)
+                {
+                    _SaveDataPaths = new string[MaxSaveFileCount];
+
+                    // Application.persistentDataPath is not known at compile time and must be called after program starts.
+                    // That is why we cannot just assign the strings as consts.
+
+                    for (int i = 1; i <= MaxSaveFileCount; i++)
+                    {
+                        _SaveDataPaths[i] = Application.persistentDataPath + "/SaveData" + i + ".json";
+                    }
+
+                    // Make sure directories exists
+                    if (_SaveDataPaths.Length > 0)
+                        // Does nothing if directory already exists, creates it if it doesn't.
+                        new FileInfo(_SaveDataPaths[0]).Directory.Create();
+                }
+
+                return _SaveDataPaths;
+            }
+        }
+        private static string[] _SaveDataPaths = null;
+
+        // Max number of files allowed
+        private static int MaxSaveFileCount = 3;
         #endregion
     }
 }
